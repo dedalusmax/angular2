212fb086ca3e8f91e23beec320a573e41d842e72
dedalusmax <dedalus123@gmail.com>
2016-05-02 18:42:44 +0200

    init

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>Weblica</h1>
+    <button md-raised-button color="primary">Characters</button>
+    <button md-icon-button>
+        <i class="material-icons md-24">favorite</i>
+    </button>
+    `
+})
+export class AppComponent { 
+    
+    
+    
+}
