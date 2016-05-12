80c2156c0ac6b394d88c949d344c522906379bd0
dedalusmax <rc@ekobit.corp>
2016-05-12 13:05:16 +0200

    router

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,22 @@
 import {Component} from 'angular2/core';
 import {ToysComponent} from './toys.component';
 import {FairsComponent} from './fairs.component';
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>Domaća radinost Kiki Art</h1>
-        <toys></toys>
-        <fairs></fairs>
-    `,
-    directives: [ToysComponent, FairsComponent]
+    templateUrl: 'app/app.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
-export class AppComponent { }
+@RouteConfig([
+    { path: '/toys', name: 'Toys', component: ToysComponent },
+    { path: '/fairs', name: 'Fairs', component: FairsComponent }
+])
+export class AppComponent {
+    
+    constructor(private router: Router) {}
+    
+    isActive(instruction: any[]): boolean {
+        return this.router.isRouteActive(this.router.generate(instruction));
+    }
+ }
